909dba7b021e7414435713d95fab0f4ce7196b5e
Alberto Maggioni <maggionialberto@gmail.com>
2018-10-01 11:03:06 +0200

    Fix group id and client id

--- src/main/java/it/intre/quotesdownloader/common/Constants.java
+++ src/main/java/it/intre/quotesdownloader/common/Constants.java
@@ -2,8 +2,8 @@ package it.intre.quotesdownloader.common;
 
 public final class Constants {
 
-    public static final String GROUP_ID = "QuoteDownloader";
-    public static final String CLIENT_ID = "QuoteDownloader";
+    public static final String GROUP_ID = "QuotesDownloader";
+    public static final String CLIENT_ID = "QuotesDownloader";
     public static final String INPUT_TOPIC = "Stocks";
     public static final String OUTPUT_TOPIC = "Quotes";
 
